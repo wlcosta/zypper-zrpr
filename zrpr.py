4d5f9314e0ca04a57a863cee0ab90c30cdc9a0da
Will <lima@linux-n3db.site>
2015-07-23 15:23:21 -0300

    first commit

---
+++ zrpr.py
@@ -0,0 +1,30 @@
+import subprocess
+repo = input('Insert repo name: ')
+string = subprocess.check_output(["zypper", "search", "--installed-only", "--repo", repo])
+unmerge = input('Do you want to remove packages that appear in some other repo?\nIf yes, type in the repository name')
+if not unmerge:
+    print("OK! No repositories will be considered...")
+
+all_description = []
+#all_description = string.replace("b'Loading repository data...", "").replace("Reading installed packages...", "").replace("\n\n\nS | Name                                 | Summary                  | Type   \n--+--------------------------------------+--------------------------+--------\n", "").replace(split('|')
+all_description = str(string).replace("b'Loading repository data...\\nReading installed packages...\\n\\nS | Name               | Summary                                                 | Type   \\n--+--------------------+---------------------------------------------------------+--------\\n", "").split('|')
+packages = []
+# How many packages are there?
+
+import math
+total_pkg = math.floor(len(all_description)/3)
+aid = 1
+for idx,notSureIfPackage in enumerate(all_description):
+    
+    while aid < len(all_description):
+        packages.append(all_description[aid].replace(" ", ""))
+        aid = aid+3
+    
+    packages
+    #print("end")
+
+zypp = ""
+for pkg in packages:
+    zypp = zypp+" "+pkg
+
+print("sudo zypper rm"+zypp)
