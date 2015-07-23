c5b401b5a77f23317644f635f5da10f6c67ebf31
Will <lima@linux-n3db.site>
2015-07-23 15:45:26 -0300

    changed some stuff

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# zypper-zrpr
-zrpr is a script for removing all packages from a repository in (open)SUSE
+# zrpr
+**zrpr** is a **z**ypper tool to **r**emove all **p**ackages from a specific **r**epository. I know, it's a horrible name.
+
+Since zypper doesn't remove the repo packages when you do **zypper rr** I made this script. Please notice that **this script won't issue any commands**, it will only **print the command line** to remove all packages.
+
+## Usage
+
+Let's say you want to remove **games** repository from your beautiful no-gaming openSUSE, but you also want to remove all packages from this repo. Instead of typing all of the packages in the command line, just run **zrpr.py**. It will ask you this:
+
+> Hello there, my fellow friend! Please, tell me, which repository are you removing today?
+
+And you'll answer, since you're a nice person.
+
+> games
+
+And it will say:
+
+> sudo zypper rm libIL1 libSDL2-2_0-0 libSDL_image-1_2-0 libSDL_net-1_2-0 libSDL_sound-1_0-1 libluajit-5_1-2 libphysfs1 xboard
+
+*because these are my packages installed from **games** repo*
+
+Just copy and paste these in your terminal, and be happy! :+1:
--- zrpr.py
+++ zrpr.py
@@ -1,10 +1,12 @@
+#!/usr/bin/python3
 import subprocess
-repo = input('Insert repo name: ')
+repo = str(input('Insert repo name: '))
 string = subprocess.check_output(["zypper", "search", "--installed-only", "--repo", repo])
-unmerge = input('Do you want to remove packages that appear in some other repo?\nIf yes, type in the repository name')
+'''unmerge = str(input('Do you want to remove packages that appear in some other repo?\nIf yes, type in the repository name'))
 if not unmerge:
     print("OK! No repositories will be considered...")
 
+'''
 all_description = []
 #all_description = string.replace("b'Loading repository data...", "").replace("Reading installed packages...", "").replace("\n\n\nS | Name                                 | Summary                  | Type   \n--+--------------------------------------+--------------------------+--------\n", "").replace(split('|')
 all_description = str(string).replace("b'Loading repository data...\\nReading installed packages...\\n\\nS | Name               | Summary                                                 | Type   \\n--+--------------------+---------------------------------------------------------+--------\\n", "").split('|')
